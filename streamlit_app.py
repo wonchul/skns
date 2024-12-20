3d54b395d4d12525c3dd0e710b5c89c74a08d1a8
노원철 <wonchul.no@gmail.com>
2024-12-20 02:05:47 +0000

    231220-1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("🎈 SKNs Corporation")
+st.write("SKNs 지원하기")
+
+st.write(" 이름과 연락처를 입력해주세요.")
+
+title = st.text_input("이름")
+tel = st.text_input("전화번호")
